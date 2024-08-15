3ae35cf02a0f7c48fb31c809691c7b4fae036cc8
Yufik3317 <nohkin@inbox.ru>
2024-08-16 03:33:00 +0700

    wip: добавил node.js, проработал структуру проекта, логику авторизации

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ stompServer.on('send', (msg, headers) => {
     console.log(`Message sent to ${headers.destination}: ${msg}`);
 });
 
-//? Запуск сервера
+// Запуск сервера
 const PORT = process.env.PORT || 8082;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
