8891ee13516f9df260e3b7203eb50ba71141bc01
mark3317 <mark.nokhrin@gmail.com>
2024-08-22 17:34:59 +0300

    wip: поправил авторизацию, отредактировал .gitignore

--- src/js/logic/authrules.js
+++ src/js/logic/authrules.js
@@ -1,6 +1,6 @@
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 const loginForm = document.querySelector('.authorize-form');
 loginForm.addEventListener('submit', async (event) => {
@@ -19,7 +19,40 @@ loginForm.addEventListener('submit', async (event) => {
         return;
     }
 
-    const socket = new SockJS('http://localhost:8081/WebChat/api/ws');
+    try {
+        const response = await fetch('http://localhost:8080/api/signIn', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ login, password })
+        });
+
+        if (response.ok) {
+            const token = response.body.toString();
+            console.log('Login successful:', token);
+            localStorage.setItem('token', token);
+
+            if (token) {
+                const decodedToken = jwtDecode(token);
+                const username = decodedToken.sub; // Извлечение subject (username)
+                console.log('Username from token:', username);
+                // Дополнительные действия после успешного входа
+                connectWebsocket(username);
+            } else {
+                console.log('Token not found');
+            }
+        } else {
+            alert('Ошибка входа');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Ошибка сети');
+    }
+});
+
+function connectWebsocket(username) {
+    const socket = new SockJS('http://localhost:8080/api/ws');
     const stompClient = new Client({
         webSocketFactory: () => socket,
         debug: (str) => {
@@ -29,44 +62,11 @@ loginForm.addEventListener('submit', async (event) => {
 
     stompClient.onConnect = (frame) => {
         console.log('Connected: ' + frame);
-
         // Подписка на ответ сервера
-        stompClient.subscribe('/user/invite', (message) => {
+        stompClient.subscribe(`/user/${username}/invite`, (message) => {
             const response = JSON.parse(message.body);
             alert(response.message);
         });
-
-        // Извлечение токена из localStorage (или другого источника)
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decodedToken = jwt_decode(token);
-            const username = decodedToken.sub; // Извлечение subject (username)
-            console.log('Username from token:', username);
-        } else {
-            console.log('Token not found');
-        }
     };
-
     stompClient.activate();
-
-    try {
-        const response = await fetch('http://localhost:8081/WebChat/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ login, password })
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Login successful:', data);
-            // Дополнительные действия после успешного входа
-        } else {
-            alert('Ошибка входа');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Ошибка сети');
-    }
-});
+}
